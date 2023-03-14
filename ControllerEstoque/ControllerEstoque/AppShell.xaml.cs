383eea1e07ac54b07352cb9cb72a9a8037171f9e
🥷 ~Pedro Arruda <pedrohendp@gmail.com>
2023-03-14 00:33:09 -0300

    second

--- ControllerEstoque/ControllerEstoque/AppShell.xaml.cs
+++ ControllerEstoque/ControllerEstoque/AppShell.xaml.cs
@@ -6,5 +6,24 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 		Routing.RegisterRoute(nameof(ListDetailDetailPage), typeof(ListDetailDetailPage));
+		SaudacaoAtual = Saudacoes();
+	}
+	public string SaudacaoAtual { get; set; }
+	private string Saudacoes()
+	{
+		var now = DateTime.Now;
+		var hour = now.Hour;
+		if (hour < 12)
+		{
+			return "Bom dia";
+		}
+		else if (hour < 18)
+		{
+			return "Boa tarde";
+		}
+		else
+		{
+			return "Boa noite";
+		}
 	}
 }
